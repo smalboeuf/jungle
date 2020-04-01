5d317a4f64eedd883e541b0503959eef2199f2f6
Sheldon Malboeuf <smalboeuf01@gmail.com>
2020-04-01 11:58:55 -0400

    Add stats to the admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
   def show
+    # Display a count of how many products are in the database
+    # Display a count of how many categories are in the database
+    @productsAmount = Product.all
+    @categoriesAmount = Category.all
+
   end
 end
